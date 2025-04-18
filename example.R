fa78e4beaba9835b9ef9ee16525c8989b7c88a6b
Adele Ribeiro <ribeiro@iminb028.localdomain>
2025-04-18 15:34:13 +0200

    updated example

--- example.R
+++ example.R
@@ -25,18 +25,44 @@ if (run_parallel) {
 # Generating true ADMG and PAG
 g_type <- "discr1_nc"
 true.admg <- getDAG(g_type)
-true.amat.pag <- getTruePAG(true.admg$dagg, verbose = TRUE)@amat
+true.amat.pag <- getTruePAG(true.admg$dagg, verbose = FALSE)@amat
 renderAG(true.amat.pag, add_index = F)
 
 
 # Generating a dataset according to the true ADMG
-data_type = "continuous"
+data_type = "mixed" #continuous
 N <- 10000
 
-set.seed(1042602069)
-dat_out <- generateDataset(adag = true.admg$dagg, N=N, type=data_type)
-dat <- dat_out$dat
+if (data_type == "continuous") {
+  set.seed(1042602069)
+  b.default <- sapply(1:nrow(dagitty::edges(true.admg$dagg)),
+                      function(x) {runif(1, -0.6, 0.6)})
+  dat_out <- generateDataset(adag = true.admg$dagg, N=N,
+                             type=data_type, b.default = b.default)
+} else if (data_type == "mixed") {
+  all_vars = names(dagitty::coordinates(true.admg$dagg)[["x"]])
+  f.args <- list()
 
+  #cur_seed <- sample(1:.Machine$integer.max, 1)
+  #set.seed(cur_seed)
+  set.seed(1412620727)
+
+  # 1: continuous; 2: binary; 3: multinominal
+  # var_levels <- sample(1:3, size=length(all_vars), replace = TRUE)
+
+  var_levels <- c(1,2,1,2,3) # for "A", "B", "Uab", "X", "Y"
+
+  for (vari in 1:length(all_vars)) {
+    var_name <- all_vars[vari]
+    f.args[[var_name]] <- list(levels = var_levels[vari])
+  }
+
+  dat_out <- generateDataset(adag = true.admg$dagg, N=N,
+                                     type=data_type, f.args=f.args,
+                                     coef_thresh = 0.1)
+}
+
+dat <- dat_out$dat
 
 # Computing conditional independence tests in parallel
 
@@ -48,8 +74,10 @@ suffStat <- getMixedCISuffStat(dat, vars_names, covs_names)
 vars_df <- dat[,vars_names, drop=FALSE]
 citestResults <- getAllCITestResults(vars_df, indepTest, suffStat,
                                      m.max=Inf, computeProbs = TRUE,
-                                     eff_size = NULL # uses max RMSE
+                                     eff_size = 0.01 # ensure a mininum effect size
+                                     #eff_size = NULL # uses max RMSE
                                      )
+
 suffStat$citestResults <- citestResults # so we don´t redo tests later
 
 # Checking faithfulness degree
@@ -88,11 +116,17 @@ end_time <- Sys.time()
 time_taken <- end_time - start_time
 time_taken
 
+fci_pag <- fit_fci@amat
 
-renderAG(fit_fci@amat, add_index = F)
+fci_metrics <- getMetrics(true.amat.pag, fci_pag,
+                          est.sepset=getPAGImpliedSepset(fci_pag),
+                          dat=NULL,  # specify dat if BIC should be computed
+                          conservative = FALSE)
 
+fci_sepset <- fixSepsetList(fit_fci@sepset)
+formatSepset(fci_sepset)
 
-
+fci_metrics$shd
 
 #################
 # Running dcFCI #
@@ -117,6 +151,28 @@ fit_dcfci$top_scoresDF
 ntop <- length(fit_dcfci$top_dcPAGs)
 ntop # number of PAGs ranked at 1st.
 
-renderAG(fit_dcfci$top_dcPAGs[[1]]$amat.pag, add_index = F)
+dcfci_pag <- fit_dcfci$top_dcPAGs[[1]]$amat.pag
+
+
+dcfci_metrics <- getMetrics(true.amat.pag, dcfci_pag,
+                          est.sepset=getPAGImpliedSepset(dcfci_pag),
+                          dat=NULL,  # specify dat if BIC should be computed
+                          conservative = FALSE)
+
+dcfci_sepset <- fit_dcfci$top_dcPAGs[[1]]$sepset
+formatSepset(dcfci_sepset)
+fci_metrics$shd
+
+
+########################
+# Printing the results #
+########################
+
+renderAG(fci_pag, add_index = F) # FCI PAG
+cat("FCI PAG's SHD: ", fci_metrics$shd, "\n")
+
+renderAG(fit_dcfci$top_dcPAGs[[1]]$amat.pag, add_index = F) # dcFCI PAG
+cat("dcFCI PAG's SHD: ", dcfci_metrics$shd, "\n")
+
 
 
